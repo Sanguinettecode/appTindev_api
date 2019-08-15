7d40a79b573b7882eeb3cdd314868fef2b2bed23
Andre Sanguinette <sanguinettecode@gmail.com>
2019-08-14 23:24:16 -0300

    adição de websocket para funcionalidade de match nas telas dos usuários logados

--- src/controllers/LikesController.js
+++ src/controllers/LikesController.js
@@ -1,6 +1,8 @@
 const Dev = require('../models/Dev')
 module.exports = {
   async store(req, res) {
+
+    console.log(req.io, req.usersConected);
     const { idDev } = req.params;
     const { user } = req.headers;
     
@@ -11,7 +13,15 @@ module.exports = {
       return res.status(400).json({error: 'Dev not exists'})
     }
     if(targetDev.likes.includes(loggedDev._id)){
-      console.log('Deu Match!')
+      const loggedSocket = req.usersConected[user];
+      const targetSocket = req.usersConected[idDev];
+
+      if(loggedSocket) {
+        req.io.to(loggedSocket).emit('match', loggedDev);
+      }
+      if(targetSocket) {
+        req.io.to(targetSocket).emit('match', targetDev);
+      }
     }
 
     loggedDev.likes.push(targetDev._id);
--- src/server.js
+++ src/server.js
@@ -3,12 +3,27 @@ const routes = require('./routes');
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const usersConected = {}
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+    usersConected[user] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.usersConected = usersConected;
+  return next();
+})
 mongoose.connect('mongodb://andretrindade:Galileu123@ds261077.mlab.com:61077/omnistack', {
   useNewUrlParser: true
 })
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
